f188a185103a8954f7cd60184b7f311a9e987be4
APetrov <a.petrov1024@yandex.ru>
2023-07-22 18:42:43 +0300

    Localization

---
+++ src/DrugstoreWarehouse.Domain.Shared/Localization/LocalizerKeys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrugstoreWarehouse.Localization
+{
+    public static class LocalizerKeys
+    {
+        public static class Errors
+        {
+            public const string ProductNotFound = "Errors:ProductNotFound";
+            public const string WarehouseNotFound = "Errors:WarhouseNotFound";
+            public const string BatchNotFound = "Errors:BatchNotFound";
+            public const string DrugstoreNotFound = "Errors:DrugstoreNotFound";
+        }
+    }
+}
