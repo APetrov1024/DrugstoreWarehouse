352623133ae7e17b29f8241dfc140caa0e79d90c
APetrov <a.petrov1024@yandex.ru>
2023-08-06 20:39:34 +0300

    Master-details logic moved to MasterDetailsManager

--- src/DrugstoreWarehouse.Web/Pages/Warehouses/Warehouses.js
+++ src/DrugstoreWarehouse.Web/Pages/Warehouses/Warehouses.js
@@ -6,6 +6,17 @@ let selectedWhId = null;
 const batchesAppService = drugstoreWarehouse.batches.batches;
 const warehousesAppService = drugstoreWarehouse.warehouses.warehouses;
 
+const masterDetailsManager = new MasterDetailsManager({
+    masterAppService: warehousesAppService,
+    onClick: onSelectWh,
+    onDelete: onDeleteWh,
+    onEditBtnClick: id => createUpdateWarehouseModal.open({ warehouseId: id }),
+    deleteConfirmationMessage: {
+        header: L('Message:Warehouse:DeleteConfirmHeader'),
+        text: L('Message:Warehouse:DeleteConfirmMessage')
+    },
+});
+
 const createUpdateWarehouseModal = new abp.ModalManager({
     viewUrl: abp.appPath + 'Warehouses/CreateUpdateWarehouseModal',
     modalClass: 'CreateUpdateWarehouseModal'
@@ -26,81 +37,27 @@ document.getElementById('addBatchBtn').addEventListener('click', function () {
 
 createUpdateWarehouseModal.onResult(function (result) {
     if (result.actionType === 'create') {
-        addWarehouseListItem(result);
+        masterDetailsManager.add(result.id, result.name);
     } else {
-        var item = document.querySelector(`.master-details__master-list-item[data-id="${result.id}"]`);
-        item.querySelector('.master-details__master-list-item__text').innerHTML = result.name;
+        masterDetailsManager.update(result.id, result.name);
     }
 });
-
-function addWarehouseListItem(data) {
-    const whItem = document.createElement('div');
-    whItem.classList.add('list-group-item', 'list-group-item-action', 'master-details__master-list-item')
-    whItem.setAttribute('data-id', data.id);
-    whItem.innerHTML = `<span class="master-details__master-list-item__text">${data.name}</span>
-                        <span class="master-details__master-list-item__toolbar">
-                            <span class="master-details__master-list-item__tool-btn master-details__master-list-item__edit-btn"><i class="fas fa-edit"></i></span>
-                            <span class="master-details__master-list-item__tool-btn master-details__master-list-item__delete-btn"><i class="fas fa-trash"></i></span>
-                        </span>`;
-    document.querySelector('.master-details__master-list-container > .list-group').appendChild(whItem);
-    addWarehouseListItemEventListeners(whItem);
-}
-
-function addWarehouseListItemEventListeners(item) {
-    const id = item.getAttribute('data-id');
-
-    item.addEventListener('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        const oldSelected = document.querySelector('.master-details__master-list-item.active')
-        if (oldSelected) oldSelected.classList.remove('active');
-        const newSelected = e.target.closest('.master-details__master-list-item');
-        newSelected.classList.add('active');
-        loadDetails(newSelected.getAttribute('data-id'))
-    });
-    item.querySelector('.master-details__master-list-item__edit-btn').addEventListener('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        createUpdateWarehouseModal.open({ warehouseId: id });
-    });
-    item.querySelector('.master-details__master-list-item__delete-btn').addEventListener('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        deleteWhItem(id)
-    });
-}
-
-function deleteWhItem(id) {
-    abp.message.confirm(L('Message:Warehouse:DeleteConfirmMessage'), L('Message:Warehouse:DeleteConfirmHeader'))
-        .then(function (confirmed) {
-            if (confirmed) {
-                abp.ui.setBusy();
-                warehousesAppService.delete(id)
-                    .done(function () {
-                        var item = document.querySelector(`.master-details__master-list-item[data-id="${id}"]`);
-                        if (item.classList.contains('active')) {
-                            selectedWhId = null;
-                            batchesTable.destroy();
-                            batchesTable = null;
-                            document.querySelector('.select-wh-alert').classList.remove('hidden');
-                            document.getElementById('addBatchBtn').setAttribute('disabled', true);
-
-                        }
-                        item.remove();
-                        abp.ui.clearBusy();
-                    })
-                    .catch(err => handleError(err));
-            }
-        });
-}
 
 createUpdateBatchModal.onResult(function (result) {
     batchesTable.updateOrAddData([result]);
 });
 
-document.querySelectorAll('.master-details__master-list-item').forEach(x => addWarehouseListItemEventListeners(x));
+function onDeleteWh(id, isActive) {
+    if (isActive) {
+        selectedWhId = null;
+         batchesTable.destroy();
+         batchesTable = null;
+         document.querySelector('.select-wh-alert').classList.remove('hidden');
+         document.getElementById('addBatchBtn').setAttribute('disabled', true);
+    }
+}
 
-function loadDetails(id) {
+function onSelectWh(id) {
     selectedWhId = id;
     if (batchesTable === null) {
         // первоначальный выбор склада, инициализируем интерфейс для партий
@@ -127,8 +84,8 @@ function createBatchesTable() {
                     { btnClass: 'btn-delete', iconClass: 'fas fa-trash' },
                 ],
                 {
-                    'btn-edit': editClicked,
-                    'btn-delete': deleteClicked
+                    'btn-edit': editBatchClicked,
+                    'btn-delete': deleteBatchClicked
                 }
             ),
             { title: L('FieldName:Batch:ProductName'), field: 'productName', headerFilter: 'input' },
@@ -152,12 +109,12 @@ function batchesQuery(url, config, params) {
     });
 }
 
-function editClicked(cell) {
+function editBatchClicked(cell) {
     let rowData = cell.getRow().getData();
     createUpdateBatchModal.open({ warehouseId: selectedWhId, batchId: rowData.id })
 }
 
-function deleteClicked(cell) {
+function deleteBatchClicked(cell) {
     abp.message.confirm(L('Message:Batch:DeleteConfirmMessage'), L('Message:Batch:DeleteConfirmHeader'))
         .then(function (confirmed) {
             if (confirmed) {
--- src/DrugstoreWarehouse.Web/wwwroot/js/MasterDetails.js
+++ src/DrugstoreWarehouse.Web/wwwroot/js/MasterDetails.js
@@ -1,3 +1,113 @@
-const contentHeight = calculateContentHeight();
-document.querySelector('.master-details__master-list-container').setAttribute('style', `height:${contentHeight}px;  max-height:${contentHeight}`);
-document.querySelector('.master-details__details-container').setAttribute('style', `height:${contentHeight}px; max-height:${contentHeight}`);
+class MasterDetailsManager {
+    #selectedId = null;
+    #masterListElement = null;
+    #detailsContainer = null;
+    #deleteConfirmationMessage = { header: "Are you sure?", text: '', };
+    #masterAppService = null;
+    #renderItem = this.#renderItem_default;
+    #onDelete = (id, isActive) => { };
+    #onClick = (id) => { };
+    #onEditBtnClick = (id) => { };
+
+
+    constructor(options) {
+        this.#masterListElement = document.querySelector('.master-details__master-list-container > .list-group');
+        if (!this.#masterListElement) throw new Error('Master list not found');
+        this.#detailsContainer = document.querySelector('.master-details__details-container')
+        if (!this.#detailsContainer) throw new Error('Details container not found');
+
+        const contentHeight = calculateContentHeight();
+        document.querySelector('.master-details__master-list-container').setAttribute('style', `height:${contentHeight}px;  max-height:${contentHeight}`);
+        document.querySelector('.master-details__details-container').setAttribute('style', `height:${contentHeight}px; max-height:${contentHeight}`);
+
+        if (options.deleteConfirmationMessage.header) this.#deleteConfirmationMessage.header = options.deleteConfirmationMessage.header;
+        if (options.deleteConfirmationMessage.text) this.#deleteConfirmationMessage.header = options.deleteConfirmationMessage.text;
+
+        this.#masterAppService = options.masterAppService;
+        if (!this.#masterAppService) throw new Error("Master application service must be set");
+
+        if (typeof (options.renderItem) === 'function') this.#renderItem = options.renderItem;
+        if (typeof (options.onClick) === 'function') this.#onClick = options.onClick;
+        if (typeof (options.onDelete) === 'function') this.#onDelete = options.onDelete;
+        if (typeof (options.onEditBtnClick) === 'function') this.#onEditBtnClick = options.onEditBtnClick;
+
+        this.#initExistingListItems();
+    }
+
+    #initExistingListItems() {
+        this.#masterListElement.querySelectorAll('.master-details__master-list-item').forEach(x => this.#addItemEventHandlers(x));
+    }
+
+    get selectedId() {
+        return this.#selectedId;
+    }
+
+    #renderItem_default(id, text) {
+        const item = document.createElement('div');
+        item.classList.add('list-group-item', 'list-group-item-action', 'master-details__master-list-item')
+        item.setAttribute('data-id', id);
+        item.innerHTML = `<span class="master-details__master-list-item__text">${text}</span>
+                          <span class="master-details__master-list-item__toolbar">
+                              <span class="master-details__master-list-item__tool-btn master-details__master-list-item__edit-btn"><i class="fas fa-edit"></i></span>
+                              <span class="master-details__master-list-item__tool-btn master-details__master-list-item__delete-btn"><i class="fas fa-trash"></i></span>
+                          </span>`;
+        this.#masterListElement.appendChild(item);
+        return item;
+    }
+
+    add(id, text) {
+        const item = this.#renderItem(id, text);
+        this.#masterListElement.appendChild(item);
+        this.#addItemEventHandlers(item);
+        return item;
+    }
+
+    #addItemEventHandlers(item) {
+        const id = item.getAttribute('data-id');
+
+        item.addEventListener('click', e => {
+            e.preventDefault();
+            e.stopPropagation();
+            const oldSelected = this.#masterListElement.querySelector('.master-details__master-list-item.active')
+            if (oldSelected) oldSelected.classList.remove('active');
+            const newSelected = e.target.closest('.master-details__master-list-item');
+            newSelected.classList.add('active');
+            this.#selectedId = id;
+            this.#onClick(id);
+        });
+        item.querySelector('.master-details__master-list-item__edit-btn').addEventListener('click', e => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.#onEditBtnClick(id)
+        });
+        item.querySelector('.master-details__master-list-item__delete-btn').addEventListener('click', e => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.delete(id);
+        });
+    }
+
+    update(id, text) {
+        const item = this.#masterListElement.querySelector(`.master-details__master-list-item[data-id="${id}"]`);
+        item.querySelector('.master-details__master-list-item__text').innerHTML = text;
+    }
+
+    delete(id) {
+        abp.message.confirm(this.#deleteConfirmationMessage.text, this.#deleteConfirmationMessage.header)
+            .then(confirmed => {
+                if (confirmed) {
+                    abp.ui.setBusy();
+                    this.#masterAppService.delete(id)
+                        .done(() => {
+                            const item = this.#masterListElement.querySelector(`.master-details__master-list-item[data-id="${id}"]`);
+                            this.#onDelete(item.getAttribute('data-id'), item.classList.contains('active'));
+                            item.remove();
+                            abp.ui.clearBusy();
+                        })
+                        .catch(err => handleError(err));
+                }
+            });
+    }
+
+
+}
